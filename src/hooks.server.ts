b3d1cc84b3d9ecfc2acb4d8739ed6edad677bc19
Visily <36494994+VisilyRomani@users.noreply.github.com>
2023-10-19 20:47:22 -0600

    url company

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -29,7 +29,11 @@ export const handle: Handle = async ({ event, resolve }) => {
 			event.locals.pb.authStore.clear();
 			throw redirect(303, '/login');
 		}
-		if (!event.locals.user.first_name && event.url.pathname !== '/admin/initial-setup') {
+		if (
+			!event.locals.user.first_name &&
+			!event.locals.user.company &&
+			event.url.pathname !== '/admin/initial-setup'
+		) {
 			throw redirect(303, '/admin/initial-setup');
 		}
 		// check for setup after setup is done and redirect
