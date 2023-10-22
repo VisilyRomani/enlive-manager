9522735937bf8a505386360c5dd9659f314fc9ee
Visily <36494994+VisilyRomani@users.noreply.github.com>
2023-10-22 10:24:42 -0600

    styled side nav

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,25 +1,103 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { getDrawerStore } from '@skeletonlabs/skeleton';
 	const drawerStore = getDrawerStore();
 	const navClose = () => {
 		drawerStore.close();
 	};
-	$: user = $page.data.user;
+
+	const logout = async () => {
+		// console.log($page.data);
+		try {
+			await fetch('/auth/logout', { method: 'GET' });
+			goto('/login');
+		} catch (err) {
+			if (err instanceof Error) {
+				console.error(err.message);
+			}
+		}
+	};
+	$: console.log($page.url.pathname);
 </script>
 
-<nav class="list-nav p-4">
+<nav class="list-nav p-4 flex flex-col justify-between h-full">
 	<ul>
-		{#if user.company}
-			<li><a href="/admin/dashboard" on:click={navClose}>Dashboard</a></li>
-			<li><a href="/admin/client" on:click={navClose}>Client</a></li>
-			<li><a href="/admin/jobs" on:click={navClose}>Jobs</a></li>
-			<li><a href="/admin/schedule" on:click={navClose}>Schedule</a></li>
-			<li><a href="/admin/invoice" on:click={navClose}>Invoice</a></li>
-			<li><a href="/app/daily" on:click={navClose}>App Jobs</a></li>
-		{:else}
-			<li><a href="/admin/company" on:click={navClose}>Company</a></li>
-		{/if}
-		<li><a href="/admin/settings" on:click={navClose}>Settings</a></li>
+		<li>
+			<a
+				href="/admin/dashboard"
+				class="!rounded-md {$page.url.pathname === '/admin/dashboard'
+					? '!bg-primary-900 text-cyan-50'
+					: ''}"
+				on:click={navClose}>Dashboard</a
+			>
+		</li>
+		<li>
+			<a
+				class="!rounded-md {$page.url.pathname === '/admin/client'
+					? '!bg-primary-900 text-cyan-50'
+					: ''}"
+				href="/admin/client"
+				on:click={navClose}>Client</a
+			>
+		</li>
+		<li>
+			<a
+				class="!rounded-md {$page.url.pathname === '/admin/jobs'
+					? '!bg-primary-900 text-cyan-50'
+					: ''}"
+				href="/admin/jobs"
+				on:click={navClose}>Jobs</a
+			>
+		</li>
+		<li>
+			<a
+				class="!rounded-md {$page.url.pathname === '/admin/schedule'
+					? '!bg-primary-900 text-cyan-50'
+					: ''}"
+				href="/admin/schedule"
+				on:click={navClose}>Schedule</a
+			>
+		</li>
+		<li>
+			<a
+				class="!rounded-md {$page.url.pathname === '/admin/invoice'
+					? '!bg-primary-900 text-cyan-50'
+					: ''}"
+				href="/admin/invoice"
+				on:click={navClose}>Invoice</a
+			>
+		</li>
+		<li>
+			<a
+				class="!rounded-md {$page.url.pathname === '/app/daily'
+					? '!bg-primary-900 text-cyan-50'
+					: ''}"
+				href="/app/daily"
+				on:click={navClose}>App Jobs</a
+			>
+		</li>
+		<li>
+			<a
+				class="!rounded-md {$page.url.pathname === '/admin/settings'
+					? '!bg-primary-900 text-cyan-50'
+					: ''}"
+				href="/admin/settings"
+				on:click={navClose}>Settings</a
+			>
+		</li>
+	</ul>
+	<ul>
+		<li>
+			<button class="btn w-full variant-outline-tertiary" on:click={logout} on:click={navClose}
+				>Logout</button
+			>
+		</li>
 	</ul>
 </nav>
+
+<style>
+	a {
+		transition: ease 0.4s !important;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,9 +23,7 @@
 		</figure>
 		<!-- / -->
 		<div class="flex justify-center space-x-2">
-			<a class="btn variant-filled" href="https://skeleton.dev/" target="_blank" rel="noreferrer">
-				Launch Documentation
-			</a>
+			<a class="btn variant-filled" href="/login"> Login </a>
 		</div>
 		<div class="space-y-2">
 			<p>Try editing the following:</p>
