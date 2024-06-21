8883f28bfdc4889fba61e48fa63ef099d0f7207a
Michael <michael4161@hotmail.com>
2024-06-20 20:54:55 -0600

    database shit itself

---
+++ server/pb_migrations/1718938348_updated_address.js
@@ -0,0 +1,76 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("6c0af6kmb5jswbv")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "5d5xfjkm",
+    "name": "lat",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "eoogzzo1",
+    "name": "lng",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("6c0af6kmb5jswbv")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "5d5xfjkm",
+    "name": "lat",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "eoogzzo1",
+    "name": "lng",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
